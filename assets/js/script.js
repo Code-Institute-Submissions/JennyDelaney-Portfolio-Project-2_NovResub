ad0effa69da9340aa906aae8e2f1db95bb0ecc7c
Jenny Delaney <jennyelewis29@gmail.com>
2022-09-26 15:44:22 +0000

    Add modal pop-up function for How to play

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+// Game functions - code taken from https://marina-ferreira.github.io/tutorials/js/memory-game/ and amended
+
 const cards = document.querySelectorAll('.memory-card');
 
 let hasFlippedCard = false;
@@ -23,8 +25,7 @@ function flipCard() {
 
 function checkForMatch () {
   let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
-  isMatch ? disableCards() : unflipCards();
-  
+  isMatch ? disableCards() : unflipCards();  
 }
 
 function disableCards() {
@@ -58,3 +59,29 @@ function resetBoard () {
 })();
 
 cards.forEach(card => card.addEventListener('click', flipCard));
+
+// How to play Modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
